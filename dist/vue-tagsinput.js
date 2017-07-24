df6bc6dac3b4eaea78cbc6c72a7cca35c9496f87
elBarkey <elbarkey@gmail.com>
2017-07-24 17:05:33 +0700

    log event testing from forked repo

--- dist/vue-tagsinput.js
+++ dist/vue-tagsinput.js
@@ -565,10 +565,12 @@
 	    removeTag: function removeTag(index) {
 	      if (index >= 0 && !this.tags[index].readOnly) {
 	        this.$emit('tags-change', this.parId, index, undefined);
+	        console.log('some tags removed!');
 	      }
 	    },
 	    insertTag: function insertTag(index, text) {
 	      this.$emit('tags-change', this.parId, index, text);
+	      console.log('some tags added!');
 	    },
 	    activeOther: function activeOther(index) {
 	      if (index >= 0 && index <= this.length) {
